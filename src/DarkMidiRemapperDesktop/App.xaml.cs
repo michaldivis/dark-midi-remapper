33d82290b34c3372bb30cbe0ce59a18f2433cea8
Michal Diviš <michall.divis@gmail.com>
2022-06-28 14:10:01 +0200

    licensing syncfusion

--- src/DarkMidiRemapperDesktop/App.xaml.cs
+++ src/DarkMidiRemapperDesktop/App.xaml.cs
@@ -1,6 +1,21 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.Configuration;
+using System.Windows;
 
 namespace DarkMidiRemapperDesktop;
 public partial class App : Application
 {
+    public App()
+    {
+        RegisterSyncfusion();
+    }
+
+    private void RegisterSyncfusion()
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddUserSecrets<App>();
+        var configuration = builder.Build();
+
+        var licenseKey = configuration["SyncfusionLicenseKey"];
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(licenseKey);
+    }
 }
